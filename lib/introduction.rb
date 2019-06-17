6256deabf04c308b9fcf0dafa3d6be9e05d2b0f9
tyler knapp <tgknapp11@gmail.com>
2019-06-17 17:15:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,12 +1,12 @@
 def introduction(name)
-  p "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}"
 end
 
 def introduction_with_language(name, language)
-  p "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language_optional(name, language)
   language = "Ruby"
-  p "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
